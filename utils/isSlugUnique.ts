e3f26320088d2282b0fa1f5e80b14ed742f2ce8b
Kristoffer <kunkristoffer@gmail.com>
2025-05-06 15:08:22 +0200

    Adding util fn for checking slug uniqueness

---
+++ utils/isSlugUnique.ts
@@ -0,0 +1,16 @@
+import { Slug } from 'sanity';
+import { SlugValidationContext } from '@sanity/types';
+
+export async function isUniqueAcrossAllDocuments(slug: Slug, context: SlugValidationContext) {
+  const { document, getClient } = context;
+  const client = getClient({ apiVersion: '2022-12-07' });
+  const id = document?._id.replace(/^drafts\./, '');
+  const params = {
+    draft: `drafts.${id}`,
+    published: id,
+    slug,
+  };
+  const query = `!defined(*[!(_id in [$draft, $published]) && slug.current == $slug][0]._id)`;
+  const result = await client.fetch(query, params);
+  return result;
+}
