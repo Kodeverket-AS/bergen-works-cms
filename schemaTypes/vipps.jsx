3357d8575dd7888178c25b3d3b7c513c7c47ca1f
Monika <maam092024@gmail.com>
2025-06-10 14:10:30 +0200

    VippsCard update

--- schemaTypes/vipps.jsx
+++ schemaTypes/vipps.jsx
@@ -1,45 +1,65 @@
 
 
 export default {
-  name: "vippsCard",
-  title: "Vipps Card",
-  type: "document",
+  name: 'vippsCard',
+  title: 'Vipps Card',
+  type: 'document',
   fields: [
     {
-      name: "serviceTitle",
-      title: "Service Title",
-      type: "string",
+      name: 'serviceTitle',
+      title: 'Service Title',
+      type: 'string',
       validation: (Rule) => Rule.required(),
     },
     {
-      name: "price",
-      title: "Price",
-      type: "object",
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'object',
       fields: [
         {
-          name: "period",
-          title: "Period",
-          type: "string",
+          name: 'number',
+          title: 'Number',
+          type: 'number',
+          validation: (Rule) => Rule.positive(),
+        },
+        {
+          name: 'period',
+          title: 'Period',
+          type: 'string',
           options: {
             list: [
-              { title: "Day", value: "dag" },
-              { title: "5 Days", value: "5dager" },
-            
+              {title: 'Hour/hours', value: 't'},
+              {title: 'Day', value: 'd'},
+              {title: 'Days', value: 'dager'},
+              {title: 'Week', value: 'u'},
+              {title: 'Weeks', value: 'uker'},
+              {title: 'Month', value: 'mnd'},
+              {title: 'Months', value: 'mndr'},
+              {title: 'Year', value: 'år'},
+              {title: 'Years', value: 'år'},
             ],
           },
         },
         {
-          name: "price",
-          title: "Price",
-          type: "number",
+          name: 'price',
+          title: 'Price',
+          type: 'number',
+          validation: (Rule) => Rule.positive(),
         },
       ],
+      validation: (Rule) => Rule.required(),
     },
     {
-      name: "url",
-      title: "URL",
-      type: "url",
-      description: "Link to the Vipps page",
+      name: 'url',
+      title: 'URL',
+      type: 'url',
+      description: 'Link to the Vipps page',
+      validation: (Rule) => Rule.required(),
     },
   ],
-};
+}
